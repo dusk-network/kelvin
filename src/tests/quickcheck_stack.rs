28c18b6b33a0dd598d65660a029487adefafaf7c
Kristoffer Ström <kristoffer@rymdkoloni.se>
2020-08-27 18:07:51 +0200

    Copyright annotations

--- src/tests/quickcheck_stack.rs
+++ src/tests/quickcheck_stack.rs
@@ -1,3 +1,6 @@
+// Copyright (c) DUSK NETWORK. All rights reserved.
+// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
+
 /// Model test suite for stacks
 ///
 /// Usage example: `quickcheck_stack!(|| HAMT::new());`
--- structures/hamt/src/lib.rs
+++ structures/hamt/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright (c) DUSK NETWORK. All rights reserved.
+// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
+
 //! A Hash-array mapped trie implemented on kelvin
 #![warn(missing_docs)]
 
